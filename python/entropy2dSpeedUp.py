00a6df513870bfe46392baeaf27738c3180be6e6
Bert Fan <burt.d.fan@qq.com>
2021-05-13 15:55:14 +0800

    Update entropy2dSpeedUp.py
    优化图像二维信息熵代码，提升计算速度

--- python/entropy2dSpeedUp.py
+++ python/entropy2dSpeedUp.py
@@ -5,16 +5,13 @@ from collections import Counter
 import time
 from numba import jit
 
-
 @jit(nopython=True)
 def calcIJ(img_patch):
-    total_p = img_patch.shape[0] * img_patch.shape[1]
-    if total_p % 2 != 0:
-        center_p = img_patch[int(img_patch.shape[0] / 2), int(img_patch.shape[1] / 2)]
-        mean_p = (np.sum(img_patch) - center_p) / (total_p - 1)
-        return (center_p, mean_p)
-    else:
-        pass
+    total_p = img_patch.shape[1]
+    assert total_p % 2, "error window size"
+    center_p = img_patch[:, total_p // 2]
+    mean_p = (np.sum(img_patch, axis=1) - center_p) / (total_p - 1)
+    return list(map(lambda x, y: (x, y), center_p, mean_p))
 
 
 def calcEntropy2dSpeedUp(img, win_w=3, win_h=3):
@@ -30,33 +27,16 @@ def calcEntropy2dSpeedUp(img, win_w=3, win_h=3):
 
     new_width = final_img.shape[1]
     new_height = final_img.shape[0]
-
-    # 最耗时的步骤，遍历计算二元组
-    IJ = []
-    for i in range(ext_x, new_width - ext_x):
-        for j in range(ext_y, new_height - ext_y):
-            patch = final_img[j - ext_y:j + ext_y + 1, i - ext_x:i + ext_x + 1]
-            ij = calcIJ(patch)
-            IJ.append(ij)
-
+    patch = np.zeros((img.shape[0] * img.shape[1], win_w * win_h))
+    for i in range(win_h):
+        for j in range(win_w):
+            patch[:, i * win_h + j] = final_img[i : new_height - win_h + 1 + i, j : new_width - win_w + 1 + j].flatten()
+    IJ = calcIJ(patch)
     Fij = Counter(IJ).items()
-
-    # 第二耗时的步骤，计算各二元组出现的概率
-    Pij = []
-    for item in Fij:
-        Pij.append(item[1] * 1.0 / (new_height * new_width))
-
-    H_tem = []
-    for item in Pij:
-        h_tem = -item * (np.log(item) / np.log(2))
-        H_tem.append(h_tem)
-
-    H = np.sum(H_tem)
-    return H
-
+    return np.sum(list(map(lambda x: -x * (np.log(x) / np.log(2)), map(lambda item: item[1] * 1.0 / (new_height * new_width), Fij))))
 
 if __name__ == '__main__':
-    img1 = cv2.imread("../imgs/img11.jpg", cv2.IMREAD_GRAYSCALE)
+    img1 = cv2.imread("./imgs/00000.jpg", cv2.IMREAD_GRAYSCALE)
     t1 = time.time()
     H1 = calcEntropy2dSpeedUp(img1, 3, 3)
     t2 = time.time()
